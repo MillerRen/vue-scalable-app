b2cf69a88099c973edac6b154ec251724f437420
MillerRen <ren12345@126.com>
2021-12-31 15:00:38 +0800

    build component

---
+++ bin/build-component.js
@@ -0,0 +1,33 @@
+const Service = require('@vue/cli-service/lib/Service')
+const service = new Service(process.env.VUE_CLI_CONTEXT || process.cwd())
+
+const rawArgv = process.argv.slice(2)
+const args = require('minimist')(rawArgv, {
+  boolean: [
+    // build
+    // FIXME: --no-module, --no-unsafe-inline, no-clean, etc.
+    'modern',
+    'report',
+    'report-json',
+    'inline-vue',
+    'watch',
+    // serve
+    'open',
+    'copy',
+    'https',
+    // inspect
+    'verbose'
+  ]
+})
+const command = args._[0]
+const entry = args._[1]
+
+args.target = 'lib'
+args.formats = 'umd-min'
+
+args.name = entry.replace(/(.*)\.vue$/, '$1')
+
+service.run(command, args, rawArgv).catch(err => {
+  console.error(err)
+  process.exit(1)
+})
