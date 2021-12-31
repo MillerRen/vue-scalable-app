2174b5797eb55cd91a0d3816cf6dd3cda20d6358
MillerRen <ren12345@126.com>
2021-12-31 19:18:20 +0800

    feature(ajax):request base url

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,11 @@
 import nprogress from 'nprogress'
 import axios from 'axios'
+import { BASE_URL } from '../constants/env'
 
 const instance = axios.create()
+
+instance.defaults.baseURL = BASE_URL + '/api'
+
 instance.interceptors.request.use(function (config) {
   nprogress.start()
   return config
